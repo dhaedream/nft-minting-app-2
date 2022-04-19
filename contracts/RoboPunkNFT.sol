ffafe2702a90af4b7e047e5f5a669c2bb3272659
Dhaesha M <dhaesha1@gmail.com>
2022-04-18 23:10:21 -0400

    created function to override ERC standard of locating nfts uri + enableling contract to interact with our oun uri standard

--- contracts/RoboPunkNFT.sol
+++ contracts/RoboPunkNFT.sol
@@ -29,4 +29,9 @@ contract RoboPunkNFT is ERC721, Ownable {
         baseTokenUri = baseTokenUri_;
     }
 
+    function tokenUri(uint256 tokenId_) public view override returns (string memory) {
+        require(_exists(tokenId_), 'Token does not exist!');
+        return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), ".json"));
+    }
+
 }
