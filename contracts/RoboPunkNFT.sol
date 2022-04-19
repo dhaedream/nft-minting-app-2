932fa21088cd239f70b1ddd9a533199e9bd3ddf8
Dhaesha M <dhaesha1@gmail.com>
2022-04-19 01:44:32 -0400

    completed mint function which calls the safeMint function from openzep + add to / increment the id numbers

--- contracts/RoboPunkNFT.sol
+++ contracts/RoboPunkNFT.sol
@@ -44,6 +44,12 @@ contract RoboPunkNFT is ERC721, Ownable {
         require(msg.value == quantity_ + mintPrice, 'wrong mint value');
         require(totalSupply + quantity_ <= maxSupply, 'sold out');
         require(walletMints[msg.sender] + quantity_ <+ maxPerWallet, 'exceed max wallet');
+
+        for (uint256 i =0; i < quantity_; i++) {
+            uint256 newTokenId = totalSupply + 1;
+            totalSupply++;
+            _safeMint(msg.sender, newTokenId);
+        }
     }
 
 }
